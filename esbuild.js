72e4b0935f0a7d7c6b659dd81e5dfbf68ed6d280
srmdev <68980580+srmdev@users.noreply.github.com>
2021-07-14 16:04:04 +0530

    added config

---
+++ esbuild.js
@@ -0,0 +1,13 @@
+const esbuild = require('esbuild');
+
+esbuild
+	.build({
+		entryPoints: ['src/index.ts'],
+		outdir: 'lib',
+		bundle: true,
+		minify: true,
+		splitting: true,
+		format: 'esm',
+		target: ['esnext'],
+	})
+	.catch(() => process.exit(1));
