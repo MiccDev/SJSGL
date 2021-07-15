8140872dc7422508c1c7fdc54892f3cae4a19e83
MiccDev <adnitt123@gmail.com>
2021-07-15 16:05:52 +1200

    Added title support and changed the look of the canvas

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ class Game extends GameObjectFunctions {
 	private canvas: HTMLCanvasElement = document.createElement("canvas");
 	private context: CanvasRenderingContext2D;
 	private size: Vector2;
+	private projectTitle: string;
 
-	constructor(size: Vector2) {
+	constructor(projectTitle: string, size: Vector2) {
 		super();
+		this.projectTitle = projectTitle;
 		this.size = size;
 		this.context = this.canvas.getContext("2d")!;
 
@@ -16,8 +18,13 @@ class Game extends GameObjectFunctions {
 	}
 	
 	private init(): void {
+		document.title = this.projectTitle;
 		this.canvas.width = this.size.getX();
 		this.canvas.height = this.size.getY();
+		this.canvas.style.border = '1px solid #d3d3d3';
+		this.canvas.style.background = '#f1f1f1';
+
+		document.body.append(this.canvas);
 	}
 
 }
