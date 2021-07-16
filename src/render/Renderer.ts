3884866829757fcc7be0b629d88888b8c0b9ef18
MiccDev <adnitt123@gmail.com>
2021-07-17 09:20:15 +1200

    Added roation to gameobjects.

--- src/render/Renderer.ts
+++ src/render/Renderer.ts
@@ -8,10 +8,12 @@ export default class Renderer {
 	constructor(private context: CanvasRenderingContext2D, private game: Game) {}
 
 	render(): void {
+		this.context.save();
 		var screenSize = this.game.size;
 		this.context.clearRect(0, 0, screenSize.x, screenSize.y);
 
 		this.treeSearch(this.game);
+		this.context.restore();
 	}
 
 	private treeSearch(go: GameObject | _GameObject) {
@@ -23,10 +25,18 @@ export default class Renderer {
 				? go.parent.transform.position
 				: new Vector2(0, 0);
 		if (texture) {
+			this.context.translate(
+				transform.position.x,
+				transform.position.y
+			);
+			if(transform.rotation) {
+				this.context.rotate((Math.PI / 180) * transform.rotation);
+				this.context.translate(0, 0);
+			}
 			this.context.drawImage(
 				texture.image,
-				transform.position.x + parent.x / 2,
-				transform.position.y + parent.y / 2,
+				(-transform.scale.x / 2) + parent.x / 2,
+				(-transform.scale.x / 2) + parent.y / 2,
 				transform.scale.x,
 				transform.scale.y
 			);
